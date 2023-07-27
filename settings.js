c0ea725c7ce78272e39db81201380d1c26e289af
Sreejit sarkar <107984412+Official-Rup@users.noreply.github.com>
2023-07-27 10:38:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['918617845264'] //ur owner number
+global.ownernomer = "918617845264" //ur owner number2
+global.ownername = "🦄❥𝐋𝒐𝒗𝒆✯∜𝒃𝒊𝒕𝒆!" //ur owner name
+global.ytname = "YT: " ❥𝐋𝒐𝒗𝒆✯∜𝒃𝒊𝒕𝒆❤️ //ur yt chanel name
+global.socialm = "GitHub: Official-Rup" //ur github or insta name
+global.location = "India, kolkata, west bengal" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "❥𝐋𝒐𝒗𝒆✯∜𝒃𝒊𝒕𝒆!🪸"
+global.ownernumber = '918617845264'
+global.ownername = '🦄드림 가이 ❥𝐋𝒐𝒗𝒆✯∜𝒃𝒊𝒕𝒆!'
+global.ownerNumber = ["918617845264@s.whatsapp.net"]
+global.ownerweb = "❥𝐋𝒐𝒗𝒆✯∜𝒃𝒊𝒕𝒆!"
+global.websitex = "❥𝐋𝒐𝒗𝒆✯∜𝒃𝒊𝒕𝒆!"
+global.wagc = "❥𝐋𝒐𝒗𝒆✯∜𝒃𝒊𝒕𝒆!"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.wm = "❥𝐋𝒐𝒗𝒆✯∜𝒃𝒊𝒕𝒆! Bot Inc."
+global.botscript = '' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "🦄드림 가이 DADDY/RUP❤️\n\n"
+global.creator = "918617845264@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
